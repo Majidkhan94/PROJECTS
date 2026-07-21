e26c01b0a28a7c66912d25b8f58129d274603ff8
majid <majidkhan.dev94@gmail.com>
2026-07-21 11:46:26 +0500

    User Registeration Completed with Role Access and RefreshToken Features

--- ApexSound/Backend/Backend/Users/IUserRepo.cs
+++ ApexSound/Backend/Backend/Users/IUserRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace Backend.Users
+﻿using Backend.Users;
+namespace Backend.Users
 {
     public interface IUserRepo
     {
+        Task<UserRegDTO> userRegisteration(UserRegDTO userregDTO);
+        Task<UserLogDTO> userLogin(UserLogDTO userlogDTO);
+        Task<UserProDTO> userProfile(int Id, UserProDTO userproDTO);
+        Task<bool> userDelete(int Id);
     }
 }
